0498e193b99410ae2aea80d54c40b1fdab2b0108
RojanShrestha77 <shresrozan555@gmail.com>
2026-04-19 18:25:48 +0545

    pose detection

---
+++ 3_live_detection.py
@@ -0,0 +1,5 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import pickle  # its used to saved you trained machine learning model into a file
+import os  # used to interact with you rcomputes operaitng system
